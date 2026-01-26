74c95e554ca1b2914d5704e0e56a7b11389dc686
buigiangit <60641364+buigiangit@users.noreply.github.com>
2026-01-26 22:12:00 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "name": "telegram-news-worker",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "better-sqlite3": "^11.10.0",
+    "dotenv": "^16.4.5",
+    "node-cron": "^3.0.3",
+    "node-fetch": "^3.3.2",
+    "rss-parser": "^3.13.0"
+  }
+}
